e5a8c9c4f1c38ae7df3954f988f88f0c2c01d370
Aswanth Haridas <57171141+ashhwanth@users.noreply.github.com>
2023-01-15 10:54:16 +0530

    Update merge2.c

--- merge2.c
+++ merge2.c
@@ -1,35 +1,30 @@
-#include <stdio.h> 
- 
-int main() { 
-   int arr1 [10], arr2 [10], arr3 [20]; 
-   int i, n1, n2, m, index=0; 
-   printf("\n Enter the number of elements in array 1: "); 
-   scanf("%d", &n1); 
-   printf("\n\n Enter the Elements of the first array"); 
-   printf("\n ****************************"); 
-   for(i=0;i<n1;i++)  { 
-     scanf ("%d",&arr1[i]); 
-   } 
-   printf("\n Enter the number of elements in array 2: "); 
-   scanf ("%d", &n2 ); 
-   printf("\n\n Enter the Elements of the second array"); 
-   printf("\n ****************************"); 
-   for(i=0;i<n2;i++) { 
-      scanf ("%d", &arr2[i]); 
-      m = n1+n2; 
-   } 
-   for(i=0;i<n1;i++) { 
-     arr3[index]=arr1[i]; 
-     index++; 
-   } 
-   for(i=0;i<n2;i++) { 
-      arr3[index]=arr2[i]; 
-      index++; 
-   } 
-   printf ("\n\n The merged array is"); 
-   printf ("\n ******************** " ); 
-   for(i=0;i<m;i++) { 
-     printf("\t\n Arr[%d] = %d", i, arr3[i]); 
-   } 
-     return 0; 
- }
+/* a1=1 2 3 4
+a2= 5 6 7 8 
+merge=1 5 2 6 3 7 4 8 */
+#include<stdio.h>
+void main(){
+int ar[10],b[10],c[20],i,n,m,j;
+printf("Enter the size of array:");
+scanf("%d",&m);
+/* printf("Enter the size of second array:");
+scanf("%d",&n);*/
+printf("Enter elements into first array:");
+for(i=0;i<m;++i)
+scanf("%d",&ar[i]);
+printf("Enter elements into second array:");
+for(i=0;i<m;++i)
+scanf("%d",&b[i]);
+printf("Array1:");
+for(i=0;i<m;++i)
+printf("%d\t",ar[i]);
+printf("\nArray2:");
+for(i=0;i<m;++i)
+printf("%d\t",b[i]);
+for(i=0,j=0;i<m;++i,++j){
+c[j]=ar[i];
+c[++j]=b[i];
+}
+printf("\nMerged Array:");
+for(i=0;i<m+m;++i)
+printf("%d\t",c[i]);
+} 
